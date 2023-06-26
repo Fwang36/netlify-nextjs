22e6d38651b2113777e82207d437d461ee77e19c
Francis Wang <franciswang@NJKNHVVGV9.local>
2023-06-26 16:24:07 -0400

    change dsn

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -8,7 +8,7 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 
   Sentry.init({
-    dsn: SENTRY_DSN || 'https://fd28336e6c92410386c2ffffe4d3b7c2@o1407376.ingest.sentry.io/4504089864830976',
+    dsn: SENTRY_DSN || 'https://aea7efac19544416b6a60adf127567aa@o1407376.ingest.sentry.io/4505427699564544',
 
     release: "aldenRelease",
     environment: "testing",
--- sentry.server.config.js
+++ sentry.server.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://fd28336e6c92410386c2ffffe4d3b7c2@o1407376.ingest.sentry.io/4504089864830976',
+  dsn: SENTRY_DSN || 'https://aea7efac19544416b6a60adf127567aa@o1407376.ingest.sentry.io/4505427699564544',
   debug:true,
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
